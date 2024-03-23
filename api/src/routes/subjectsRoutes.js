cb44e59990b9fbe45cad0ca11b3ee9099d26af3c
Suraj-Upadhye <s.upadhye6782@gmail.com>
2024-03-23 17:00:19 +0530

    "Removed some old files and routes"

--- api/src/routes/subjectsRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/teacherRoutes.js
-import  express from 'express';
-import { addSubjects } from  "../controllers/subjectsController.js";
-
-const router = express.Router();
-
-router.post('/addSubjects', addSubjects);
-
-export default router;
--- api/src/utils/success.js
+++
@@ -1,8 +0,0 @@
-export const CreateSuccess = (statusCode, successMessage, data) =>{
-    const successObj = {
-        status: statusCode,
-        message: successMessage,
-        data: data ? data : {}  // if there is any data passed in the function parameters then it will be added to the 'data' property of our 
-    }
-    return successObj;
-}
